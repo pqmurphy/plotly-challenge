fd523ba2b239868871c8773185deae6bae700b6b
pqm <patrickqmurphy@gmail.com>
2019-12-03 21:07:59 -0600

    done

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -7,7 +7,6 @@ function buildMetadata(sample) {
     var url = `metadata/${sample}`;
 
     d3.json(url).then(function(response) {
-      console.log(response)
 
       // Use d3 to select the panel with id of `#sample-metadata`
       var metapanel = d3.select("#sample-metadata");
@@ -17,51 +16,58 @@ function buildMetadata(sample) {
 
       // Use the list of sample names to populate the select options
         Object.entries(response).forEach(([key, value]) => {
-          console.log(value)
       // Use `Object.entries` to add each key and value pair to the panel
           var li = metapanel.append("li").text(`${key}: ${value}`);
-          
-          ///////////
-          /// THIS IS WHERE YOU WERE
-          //////////
+        });
+
+      // Use the list of sample names to populate the select options
+        Object.entries(response).forEach(([key, value]) => {
+      // Use `Object.entries` to add each key and value pair to the panel
+
           if (key === "WFREQ") {
-            var gval = value;
-            return gval;
+            var data = [
+            {
+              domain: { x: [0, 1], y: [0, 1] },
+              value: value,
+              title: { text: "Wash  Frequency" },
+              type: "indicator",
+              mode: "gauge+number",
+              gauge: {
+                axis: { range: [null, 10], tickwidth: 1, tickcolor: "darkblue" },
+                bar: { color: "darkblue" },
+                bgcolor: "white",
+                borderwidth: 2,
+                bordercolor: "gray",
+            }
+          }
+        ];
+        var offsetHeight = document.getElementById('gauge').offsetHeight;
+        var offsetWidth= document.getElementById('gauge').offsetWidth;
+
+        var layout = { width: offsetWidth, height: offsetHeight, margin: { t: 0, b: 0 } };
+        Plotly.newPlot('gauge', data, layout);
           }
 
         });
-        console.log(gval)
-
-        var gaugepan = d3.select("#gauge");
-
-        var data = [
-          {
-            domain: { x: [0, 1], y: [0, 1] },
-            value: gval,
-            title: { text: "Speed" },
-            type: "indicator",
-            mode: "gauge+number"
-          }
-        ];
-        
-        var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
-        Plotly.newPlot('myDiv', data, layout);
 
       });
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
 }
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  var url = `samples/${sample}`;
+
+  d3.json(url).then(function(response) {
+    console.log(response);
 
     // @TODO: Build a Bubble Chart using the sample data
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+
+  });
 }
 
 function init() {
